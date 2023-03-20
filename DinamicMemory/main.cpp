d8bb9b1cd8f76579d340d59add21215f6567b32f
Koros_zl65@top-academy.ru <Koros_zl65@top-academy.ru>
2023-03-21 00:54:23 +0500

    Dinamo

--- DinamicMemory/main.cpp
+++ DinamicMemory/main.cpp
@@ -2,8 +2,15 @@
 
 using namespace std;
 
+using std::cout; 
+using std::cin; 
+using std::endl; 
+
 void FillRand(int arr [], const int n);
+void FillRand(int** arr, const int rows, const int cols);
+
 void Print(int arr [], const int n);
+void Print(int** arr, const int rows, const int cols);
 
 int* Push_back(int arr[], int& n, int value);
 int* Pop_back(int arr[], int& n, int value); 
@@ -12,40 +19,71 @@ int* pop_front(int arr[], int& n, int value);
 int* insert(int arr[], int& n, int value, int index);
 int* erase(int arr[], int& n, int value, int index);
 
+//#define DIMANO_MEMORY1
+#define DINAMO_MEMORY2
+
 void main()
 {
 	setlocale(LC_ALL, "");
-	int n; 
-	cout << "Введите размер массива: "; cin >> n; 
+#ifdef DIMANO_MEMORY1
+	int n;
+	cout << "Введите размер массива: "; cin >> n;
 	int* arr = new int[n];
 	FillRand(arr, n);
-	Print(arr, n); 
+	Print(arr, n);
 	int value;
-	cout << "Введите значение добавляемого элемента: "; cin >> value; 
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
 	arr = Push_back(arr, n, value);
 	Print(arr, n);
-	
-	cout << "Удаление из массива последного элемента: " << endl; 
-	arr = Pop_back(arr, n, value); 
+
+	cout << "Удаление из массива последного элемента: " << endl;
+	arr = Pop_back(arr, n, value);
 	Print(arr, n);
 
 	cout << "Введите значение добавляемого элемента: "; cin >> value;
 	arr = push_front(arr, n, value);
-	Print(arr, n); 
+	Print(arr, n);
 
-	cout << "Удаление первого элемента из массива:" << endl; 
+	cout << "Удаление первого элемента из массива:" << endl;
 	arr = pop_front(arr, n, value);
 	Print(arr, n);
-	
-	int index; 
-	cout << "Введите значение элемента по указанному индексу: "; cin >> index;
-	arr = insert(arr, n, value,index); 
+
+	int index;
+	//cout << "Введите значение элемента по указанному индексу: "; cin >> index;
+	cout << "Введите индекс добавляемого значения:  "; cin >> index;
+	cout << "Введите значение добавляемого значения:  "; cin >> value;
+	arr = insert(arr, n, value, index);
 	Print(arr, n);
 
 	cout << "Введите значение по указанному индексу: "; cin >> index;
-	arr = erase(arr, n, value,index); 
+	arr = erase(arr, n, value, index);
 	Print(arr, n);
 	delete[] arr;
+
+#endif // DIMANO_MEMORY1
+#ifdef DINAMO_MEMORY2
+
+	int rows, cols;
+	cout << "Введите количество строк "; cin >> rows;
+	cout << "Введите количество элементов строки "; cin >> cols;
+
+	int** arr = new int* [rows];
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols] {};
+	}
+	FillRand(arr, rows, cols);
+	Print(arr, rows, cols);
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] arr[i];
+	}
+	delete[] arr;
+
+
+
+
+#endif // DINAMO_MEMORY2
 
 }
 void FillRand(int arr [], const int n)
@@ -56,6 +94,18 @@ void FillRand(int arr [], const int n)
 		// арифметику указателей и оператор разыменования  
 	}
 }
+void FillRand(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+}
+
+
 void Print(int arr [], const int n)
 {
 	for (int i = 0; i < n; i++)
@@ -65,6 +115,18 @@ void Print(int arr [], const int n)
 	}
 	cout << endl;
 }
+void Print(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
 int* Push_back(int arr[], int& n, int value) // произвести передачу по ссылке и ставим &
 {
 	//1)создать новый массив, выделить область памяти, нужного размера: 
@@ -115,6 +177,16 @@ int* push_front(int arr[], int& n, int value)
 	n++;
 	return arr;
 }
+// 2 вариант 
+//int* push_front(int arr[], int& n, int value)
+//{
+//	int* buffer = new int[n + 1];//
+//	for (int i = 0; i < n; i++)buffer[i + 1] = arr[i];// копируем со смещением
+//	delete[] arr; // удаляем массив
+//	buffer[0] = value; // помещаем добавленный элемент в начало массива
+//	n++; // 
+//	return buffer; // возвращаем массив 
+//}
 int* pop_front(int arr[], int& n, int value)
 {
 	int* crr = new int[--n];
@@ -132,6 +204,16 @@ int* insert(int arr[], int& n, int value, int index)
 	arr = buffer; 
 	return buffer; 
 }
+//int* insert(int arr[], int& n, int value, int index)
+//{
+//	int* buffer = new int[++n];
+//	for (int i = 0; i < index; i++)buffer[i] = arr[i];
+//	for (int i = index; i < n; i++)buffer[i + 1] = arr[i];
+//	delete[] arr;
+//	buffer[index] = value;
+//	n++; 
+//	return buffer; 
+//}
 int* erase(int arr[], int& n, int value, int index)
 {
 	int* buffer = new int[--n];
